ff5221cbda462ea3d80a6ef45b5a4f8eb2a9a779
niyw <nee_11235@qq.com>
2020-04-17 22:01:29 +0800

    update GPR S3S4

--- library/GPR/FileProcess_GPR_S3S4.py
+++ library/GPR/FileProcess_GPR_S3S4.py
@@ -1,7 +1,6 @@
 from docx import Document
 import os
 import csv
-import re
 from library.FileProcessBasic import FileProcessBasic
 import util
 
@@ -13,19 +12,22 @@ def check_output_file(output_path, header):
 
 class Record:
     def __init__(self, docx):
-        para_62, para_rest = self.locateParagraph(docx)
-        GSI_CHAI, GSI_INTE = self.get_GSI_CHAI_and_GSI_INTE(para_rest)
-        GSI_GPR = self.get_GSI_GPR(para_62)
-        GSI_LITH = self.get_GSI_LITH(para_rest)
-        GSI_WEA = self.get_GSI_WEA(para_rest)
-        GSI_STRU = self.get_GSI_STRU(para_rest)
-        GSI_STAB = self.get_GSI_STAB(para_rest)
+        name, GSI_INTE = self.get_cover(docx)
+        GSI_CHAI, GSI_INTE = util.parse_GSI_CHAI_and_GSI_INTE(name, GSI_INTE)
 
-        GSI_WATE = self.get_GSI_WATE(para_rest)
-        GSI_FAUL = self.get_GSI_FAUL(para_rest)
+        para_result, para_prediction = self.locate_paragraph(docx)
+        GSI_GPR = self.get_GSI_GPR(para_result)
+
+        GSI_LITH = self.get_GSI_LITH(para_prediction)
+        GSI_FAUL = self.get_GSI_FAUL(para_prediction)
+        GSI_STRU = self.get_GSI_STRU(para_prediction)
+        GSI_STAB = self.get_GSI_STAB(para_prediction)
+        GSI_WEA = self.get_GSI_WEA(para_prediction)
 
         GSI_DSCR = self.get_GSI_DSCR(docx.tables[3])
         GSI_PSRL = self.get_GSI_PSRL(docx.tables[3])
+
+        GSI_WATE = self.get_GSI_WATE(docx.tables[4])
         GSI_WATG = self.get_GSI_WATG(docx.tables[4])
 
         self.dict = {
@@ -43,29 +45,39 @@ class Record:
             "预报围岩级别": GSI_PSRL
         }
 
-    def locateParagraph(self, docx):
-        flag = 0
-        para_62 = ""
-        para_rest = ""
-        for i, p in enumerate(docx.paragraphs):
-            if p.text.startswith("6.2") and flag == 0:
-                flag = 1
-                continue
-            if flag == 1:
-                if p.text.startswith("7") or p.text.startswith("6.3"):
-                    flag = 2
-                    para_rest += p.text
-                elif p.text.startswith("图") or p.text.startswith("（本页") or p.text.startswith("  ") or p.text == "\n":
-                    continue
-                elif para_62 == "":
-                    para_62 += p.text
-                else:
-                    flag = 2
-                    para_rest += p.text
-            if flag == 2:
-                para_rest += p.text
-        return para_62, para_rest
+    def get_cover(self, docx):
+        name, GSI_INTE = None, None
+        for paragraph in docx.paragraphs:
+            if paragraph.text.startswith("隧道名称："):
+                name = paragraph.text.split("：")[1].strip()
+            if paragraph.text.startswith("预报里程："):
+                GSI_INTE = paragraph.text.split("：")[1].strip()
+            if name is not None and GSI_INTE is not None:
+                return name, GSI_INTE
 
+    def locate_paragraph(self, docx):
+        para_result = ""        # 6.2 探测结果
+        para_prediction = ""    # 6.3 前方地质情况预测
+        for i, p in enumerate(docx.paragraphs):
+            if p.text.startswith("6.2"):
+                i += 1
+                p = docx.paragraphs[i]
+                while not p.text.startswith("6.3"):
+                    if not p.text.startswith("图"):
+                        para_result += p.text
+                    i += 1
+                    p = docx.paragraphs[i]
+                i -= 1
+            elif p.text.startswith("6.3"):
+                i += 1
+                p = docx.paragraphs[i]
+                while not p.text.startswith("7"):
+                    para_prediction += p.text
+                    i += 1
+                    p = docx.paragraphs[i]
+        return para_result, para_prediction
+
+    # 掌子面桩号
     def get_GSI_CHAI_and_GSI_INTE(self, para):
         GSI_INTE = ''
         for i in range(len(para)):
@@ -78,141 +90,103 @@ class Record:
         GSI_CHAI = GSI_INTE.split("～")[0]
         return GSI_CHAI, GSI_INTE
 
+    # 地质雷达描述
     def get_GSI_GPR(self, para):
-        # 地质雷达描述
-        GSI_GPR = "无"
-        # for i in range(len(para) - 2):
-        #     if para[i:i + 3] == "电磁波":
-        #         j = i + 3
-        #         k = i + 4
-        #         times = 3
-        #         while para[j] != '，':
-        #             GSI_GPR = para[j] + GSI_GPR
-        #             j = j - 1
-        #         while para[k] != '，' or times != 0:
-        #             GSI_GPR = GSI_GPR + para[k]
-        #             k = k + 1
-        #             if para[k] == '，':
-        #                 times = times - 1
-        #         break
-        try:
-            para = para[para.find("基本规律：") + 5:]
-            GSI_GPR = para
-            return GSI_GPR
-        except:
-            return GSI_GPR
+        start = para.find("基本规律：") + 5
+        end = para.find("。", start)
+        GSI_GPR = para[start: end]
+        if GSI_GPR is None:
+            GSI_GPR = "无"
+        return GSI_GPR
 
+    # 岩性
     def get_GSI_LITH(self, para):
-        GSI_LITH = ""
-        for i in range(len(para) - 2):
-            if (para[i:i + 3] == "岩性："):
-                j = i + 3
-                while (para[j - 1] != '化'):
-                    j = j + 1
-                while (para[j] != '。'):
-                    GSI_LITH = GSI_LITH + para[j]
-                    j = j + 1
-                return GSI_LITH
-        if GSI_LITH == "":
+        keyword = "岩性："
+        start = para.find(keyword) + len(keyword)
+        end = para.find("。", start)
+        GSI_LITH = para[start: end]
+
+        keyword = "风化"
+        GSI_LITH = GSI_LITH[GSI_LITH.find(keyword) + len(keyword):]
+        if GSI_LITH is None:
             GSI_LITH = "无"
         return GSI_LITH
 
+    # 风化程度
     def get_GSI_WEA(self, para):
-        GSI_WEA = ""
-        for i in range(len(para) - 1):
-            if para[i:i + 2] == "风化":
-                GSI_WEA = para[i - 1:i + 2]
-                if para[i - 1] == "等":
-                    GSI_WEA = para[i - 2:i + 2]
-                if para[i - 2] == "～":
-                    if para[i - 3] != "等":
-                        GSI_WEA = para[i - 3:i + 2]
-                    else:
-                        GSI_WEA = para[i - 4:i + 2]
-                if para[i - 3] == "～":
-                    GSI_WEA = para[i - 4:i + 2]
-        if GSI_WEA == "":
+        keyword = "岩性："
+        start = para.find(keyword) + len(keyword)
+        end = para.find("。", start)
+        GSI_WEA = para[start: end]
+
+        keyword = "风化"
+        GSI_WEA = GSI_WEA[:GSI_WEA.find(keyword) + len(keyword)]
+        if GSI_WEA is None:
             GSI_WEA = "无"
         return GSI_WEA
 
+    # 结构构造
     def get_GSI_STRU(self, para):
-        GSI_STRU = ""
-        for i in range(len(para) - 4):
-            if para[i:i + 5] == "结构构造：":
-                j = i + 5
-                while para[j] != '。':
-                    GSI_STRU = GSI_STRU + para[j]
-                    j = j + 1
-                break
+        keyword = "结构构造："
+        start = para.find(keyword) + len(keyword)
+        end = para.find("。", start)
+        GSI_STRU = para[start: end]
         if GSI_STRU == "":
             GSI_STRU = "无"
         return GSI_STRU
 
+    # 稳定性
     def get_GSI_STAB(self, para):
-        GSI_STAB = ""
-        for i in range(len(para) - 5):
-            if (para[i:i + 6] == "稳定性分析："):
-                j = i + 6
-                while (para[j] != '。'):
-                    GSI_STAB = GSI_STAB + para[j]
-                    j = j + 1
-                break
-        if GSI_STAB == "":
+        keyword = "稳定性分析："
+        start = para.find(keyword) + len(keyword)
+        end = para.find("。", start)
+        GSI_STAB = para[start: end]
+
+        if GSI_STAB is not None:
+            GSI_STAB = GSI_STAB.split("，")[-1]
+            key = "围岩"
+            GSI_STAB = GSI_STAB[GSI_STAB.find(key) + len(key):]
+        else:
             GSI_STAB = "无"
         return GSI_STAB
 
+    # 设计围岩级别
     def get_GSI_DSCR(self, table):
         GSI_DSCR = table.rows[1].cells[3].text
         return GSI_DSCR
 
+    # 预报围岩级别
     def get_GSI_PSRL(self, table):
-        # GSI_PSRL = table.rows[1].cells[-1].text
-        # return GSI_PSRL
+        GSI_PSRL = table.rows[1].cells[-1].text
+        return GSI_PSRL
+
+    # 地下水状态描述
+    def get_GSI_WATE(self, table):
+        GSI_WATE = ""
+        for row in table.rows:
+            if row.cells[0].text.strip() == '地下水':
+                watgs = set()
+                for i in range(1, len(row.cells)):
+                    cell = row.cells[i]
+                    if "√" in cell.text:
+                        watgs.add(cell.text.replace("√", "").strip())
+                GSI_WATE = "~".join(watgs)
+                if len(watgs) > 0:
+                    break
+        if GSI_WATE == "":
+            GSI_WATE = "无"
+        return GSI_WATE
+
+    # 地下水对应等级
+    def get_GSI_WATG(self, table):
         return "无"
 
-    def get_GSI_WATE(self, para):
-        GSI_WATE = ""
-        for i in range(len(para) - 3):
-            if (para[i:i + 4] == "地下水："):
-                j = i + 4
-                while (para[j] != '。'):
-                    GSI_WATE = GSI_WATE + para[j]
-                    j = j + 1
-                return GSI_WATE
-        if GSI_WATE == "":
-            return "无"
-
-    def get_GSI_WATG(self, table):
-        GSI_WATG = ""
-        for i in range(len(table.rows)):
-            text = table.cell(i, 0).text
-            if text == '地下水':
-                tmp = list(table.rows[i].cells)
-                cols = sorted(set(tmp), key=tmp.index)
-                for col in cols:
-                    if col.text.find('√') > 0:
-                        col.text = col.text.replace('√', '').strip()
-                        GSI_WATG = col.text
-                        return GSI_WATG
-        if GSI_WATG == "":
-            GSI_WATG = "无"
-        return GSI_WATG
-
+    # 断层
     def get_GSI_FAUL(self, para):
-        # version by BuDi
-        # GSI_FAUL = '无'
-        # for i in range(len(para) - 1):
-        #     if (para[i:i + 2] == "断层"):
-        #         GSI_FAUL = "断层带"
-
-        GSI_FAUL = ''
-        for i in range(len(para) - 5):
-            if (para[i:i + 6] == "断层破碎带："):
-                j = i + 6
-                while (para[j] != '。'):
-                    GSI_FAUL = GSI_FAUL + para[j]
-                    j = j + 1
-                break
+        keyword = "断层破碎带："
+        start = para.find(keyword) + len(keyword)
+        end = para.find("。", start)
+        GSI_FAUL = para[start: end]
         if GSI_FAUL == "":
             GSI_FAUL = "无"
         return GSI_FAUL
@@ -228,19 +202,24 @@ class Processor(FileProcessBasic):
             w.writerow(record.dict)
 
     def run(self, input_path, output_path):
-        transformed = False
-        if input_path.endswith("doc"):
-            transformed = True
-            util.doc2docx(input_path)
-            input_path = input_path + "x"
-        elif not input_path.endswith("docx"):
-            return
+        files_to_process = set()
+        files_to_transform = set()
+        for file in os.listdir(input_path):
+            absolute_file_path = os.path.join(input_path, file)
+            if file.endswith(".doc"):
+                files_to_transform.add(absolute_file_path)
+            elif file.endswith(".docx"):
+                files_to_process.add(absolute_file_path)
+        files_to_delete = util.batch_doc_to_docx(files_to_transform)
+        files_to_process = files_to_process.union(files_to_delete)
 
-        docx = Document(input_path)
-        record = Record(docx)
-        self.save(output_path, record)
+        for file in files_to_process:
+            docx = Document(file)
+            record = Record(docx)
+            self.save(output_path, record)
+            print("提取完成" + file)
 
-        print("提取完成" + input_path)
-        if transformed and os.path.exists(input_path):
-            os.remove(input_path)
+        for file in files_to_delete:
+            if os.path.exists(file):
+                os.remove(file)
 
